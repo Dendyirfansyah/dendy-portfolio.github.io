a0677edacf4b22c3110cfd01d0def476a7d74938
DenskyProject <denirfansyah11@gmail.com>
2022-10-25 21:45:15 +0700

    perbaikan

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,11 +53,23 @@ skills__header.forEach((el) =>{
 
 
 /*==================== SHOW SCROLL UP ====================*/ 
-function scrollUp(){
-    const scrollUp = document.getElementById('scroll-up');
-    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
-window.addEventListener('scroll', scrollTop)
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
 /*==================== DARK LIGHT THEME ====================*/ 
